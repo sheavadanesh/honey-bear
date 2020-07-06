0875c0bffb005782ed7fabdd8c18ece55cf43753
bsillorequez <bryansillorequez@gmail.com>
2020-07-06 14:10:02 -0700

    Fix bugs for frontend

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import errors from './errors_reducers';
+import errorsReducer from './errors_reducer';
 
 const RootReducer = combineReducers({
     session,
-    errors
+    errors: errorsReducer,
 });
 
 export default RootReducer;
