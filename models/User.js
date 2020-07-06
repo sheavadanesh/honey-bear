b86a0f9b7c9931b5b0ef477465fb06a3f685b9f8
Sheava <sheavad@gmail.com>
2020-07-06 10:21:23 -0700

    update paymentInfo key

--- models/User.js
+++ models/User.js
@@ -28,7 +28,10 @@ const UserSchema = new mongoose.Schema({
     },
     paymentInfo: {
         nameOnCard: { type: String, required: false },
-        cardNumber: { type: Integer, required: false }
+        cardNumber: { type: Integer, required: false },
+        expirationDate: { type: String, required: false },
+        securityCode: { type: Integer, required: false },
+        zipCode: { type: Integer, required: false }
     }
 }, { timestamps: true })
 
