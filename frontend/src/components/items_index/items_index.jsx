489b98e32624f66ea2fb884810f28e3845906e55
bsillorequez <bryansillorequez@gmail.com>
2020-07-08 16:58:18 -0700

    Index working needs styling

--- frontend/src/components/items_index/items_index.jsx
+++ frontend/src/components/items_index/items_index.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import ItemsIndexIndv from './items_index_indv';
 
-class ItemsIndex extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// class ItemsIndex extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  //Bring this in when there is a borrow checkout page route
-  // borrowButton() {
-  //   if(this.props.session) {
-  //     return (
-  //       <div className="borrow-button-contianer">
-  //         <Link to="/borrow" className="borrow-button-link">
-  //           <h6 className="borrow-button">Borrow</h6>
-  //         </Link>        
-  //       </div>
-  //     )
-  //   }
-  // }
+//   componentDidMount() {
+//     this.setState()
+//   }
 
-  render () {
-    if(!this.props.items) return null
+//   //Bring this in when there is a borrow checkout page route
+//   // borrowButton() {
+//   //   if(this.props.session) {
+//   //     return (
+//   //       <div className="borrow-button-contianer">
+//   //         <Link to="/borrow" className="borrow-button-link">
+//   //           <h6 className="borrow-button">Borrow</h6>
+//   //         </Link>        
+//   //       </div>
+//   //     )
+//   //   }
+//   // }
 
-    const { items } = this.props;
-    debugger
+//   render () {
+//     if(!this.props.items) return null
+
+//     const { items } = this.props;
+//     debugger
+//     return (
+//       <div className='item-index'>
+//         <ul>
+//           { items.map((item, i) => <ItemsIndexIndv key={i} item={item}/>) }
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+const ItemsIndex = ({ items }) => {
     return (
       <div className='item-index'>
         <ul>
-          { items.map((item, i) => <ItemsIndexIndv key={i} item={item}/>) }
+          { items && items.map((item, i) => <ItemsIndexIndv key={i} item={item}/>) }
         </ul>
       </div>
     )
-  }
 }
 
 export default ItemsIndex
