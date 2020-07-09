36dda5b44bead694b32f1497c6a4a31933d1a4f7
Sheava <sheavad@gmail.com>
2020-07-09 13:55:18 -0700

    more css styling

--- frontend/src/components/items_index/items_index_indv.jsx
+++ frontend/src/components/items_index/items_index_indv.jsx
@@ -8,17 +8,17 @@ const ItemsIndexIndv = ({ item }) => {
             <div className='item-index-indv'>
                 <div className='left-side'>
                     <div className='photo-cont'>
-                        {/* <img className='index-item-photo' src={item.photoUrl}></img> */}
+                        <img className='index-item-photo' src={item.imageUrl}></img>
                     </div>
                 </div>
                 <div className='right-side'>
                     <span className='index-item-title'>{item.title[0].toUpperCase() + item.title.slice(1).toLowerCase()}</span>
+                    <span className='index-item-rate'>${updatedPrice} <span className='per-day'>per day</span></span>
                     <span className='index-item-category'>{item.category[0].toUpperCase() + item.category.slice(1).toLowerCase()}</span>
                     <span className='index-item-description'>
                         <span className='about-head'>About this item</span>
                         <span className='item-description-actual'>{item.description}</span>
                     </span>
-                    <span className='index-item-rate'>${updatedPrice}</span>
                 </div>
             </div>
         </li>
