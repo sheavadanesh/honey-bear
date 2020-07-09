9dce08aeed889e8cd7b6fadc796f98b605005679
bsillorequez <bryansillorequez@gmail.com>
2020-07-09 16:49:39 -0700

    Remove PublicIcon

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const passport = require('passport');
 const users = require("./routes/api/users");
 const items = require("./routes/api/items");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
@@ -31,9 +38,3 @@ app.use("/api/items", items);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
