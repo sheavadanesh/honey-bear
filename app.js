705ef88735852efdc2d02d88185ee6b5b6fd3d8e
bsillorequez <bryansillorequez@gmail.com>
2020-07-09 16:41:03 -0700

    app.js

--- app.js
+++ app.js
@@ -30,3 +30,10 @@ app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
