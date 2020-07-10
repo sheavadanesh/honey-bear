3317bf62e5107c0034c7dc726d466ff5b2be6ea1
Sheava <sheavad@gmail.com>
2020-07-10 10:19:36 -0700

    implement conditional to show error page if no items are found

--- frontend/src/components/items_index/items_index.jsx
+++ frontend/src/components/items_index/items_index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ItemsIndexIndv from './items_index_indv';
 import Map from '../map';
 import '../../scss/layout/index_items.scss'
 
 const ItemsIndex = ({ history, items }) => {
+  if (items) {
     return (
       <>
       <div className="item-index-parent-container">
@@ -18,10 +20,24 @@ const ItemsIndex = ({ history, items }) => {
           <div className="items-left-side-container">
             {items && <Map items={items} />}
           </div>
-          </div>
+        </div>
       </div>
     </>
-  );
+  )} else {
+    return (
+      <>
+        <div className="item-index-parent-container">
+          <div className="item-index">
+            <div className="sorry-container">
+                <div className='sorry'>
+                  Sorry, there are no items matching your search. Please try another search or <Link className='link-to-homepage' to='/'>return to the homepage.</Link>
+                </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  };
 }
 
 export default ItemsIndex;
