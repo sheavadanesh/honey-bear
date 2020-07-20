9178763ac4e8ef240dadfe7ddec8997718e81bb6
Ahmad Alqadhy <ahmadalqadhy@gmail.com>
2020-07-20 12:35:29 -0700

    fix index container

--- frontend/src/actions/items_index_actions.js
+++ frontend/src/actions/items_index_actions.js
@@ -3,10 +3,11 @@ import * as ItemAPIUtil from '../util/item_api_util';
 export const RECEIVE_ALL_ITEMS = 'RECEIVE_ALL_ITEMS';
 // export const RECEIVE_ITEM = 'RECEIVE_ITEM';
 
-export const receiveItems = (items) => ({
+export const receiveItems = (items) => {
+    return {
     type: RECEIVE_ALL_ITEMS,
     items
-});
+}};
 
 // const receiveItem = (item) => ({
 //     type: RECEIVE_ITEM,
@@ -15,7 +16,7 @@ export const receiveItems = (items) => ({
 
 export const fetchItems = () => dispatch => (
     ItemAPIUtil.fetchItems().then(items => dispatch(receiveItems(items)))
-);
+)
 
 // export const fetchItem = (itemId) => dispatch => (
 //     ItemAPIUtil.fetchItem(itemId).then(item => dispatch(receiveItem(item)))
