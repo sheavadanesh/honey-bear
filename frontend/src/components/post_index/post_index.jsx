37b1eebd1e2324ec254b1ff04e2a4a264ef538b6
Sheava <sheavad@gmail.com>
2020-07-20 14:57:29 -0700

    update scss for post index

--- frontend/src/components/post_index/post_index.jsx
+++ frontend/src/components/post_index/post_index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ItemsIndexIndv from '../items_index/items_index_indv';
+import '../../scss/layout/post_index.scss';
 
 class PostIndex extends React.Component {
   constructor(props) {
@@ -15,26 +16,17 @@ class PostIndex extends React.Component {
   render() {
     const { items } = this.props;
     return (
-      <div className="post-index-parent-container">
-        <div className="post-index">
-          <div className="post-right-side-container">
-            <ul className="post-right-side">
-              {items &&
-                items.map((item, i) => <ItemsIndexIndv key={i} item={item} />)}
-            </ul>
-          </div>
-          {/* <div className="post-left-side-container">
-                        <div className='map-container-div'>
-                            <div className="index-head">gear available near you</div>
-                            <div>{items && <Map items={items} />}</div>
-                        </div>
-                    </div> */}
-
-                </div>
-                <div className="post-link-to-search">
-                    <Link className='post-link-to-homepage' to='/'>Looking for something specific?</Link>
+        <div className="post-index-parent-container">
+            <div className='all-items'>all items</div>
+            <div className="post-index">
+                <div className="post-right-side-container">
+                    <ul className="post-right-side">
+                    {items &&
+                        items.map((item, i) => <ItemsIndexIndv key={i} item={item} />)}
+                    </ul>
                 </div>
             </div>
+        </div>
         )
     }
 }
